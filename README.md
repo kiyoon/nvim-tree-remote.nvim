4ca410526cc7f7884d5a87a1a31f412c63a9675c
Kiyoon Kim <yoonkr33@gmail.com>
2022-12-28 14:34:38 +0000

    Working. Final?

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 A set of [Nvim-Tree](https://github.com/nvim-tree/nvim-tree.lua) actions to open files on another remote neovim.
 
+Why? Sometimes you want to use Nvim-Tree as a standalone file browser. This way, you can make your own IDE with Tmux, for example.  
+The problem is that the Nvim-Tree doesn't interact with another Neovim which can get annoying.
+
+With this tool, you can control a remote instance of Neovim which makes it possible to use Nvim-Tree as a seamless standalone file explorer!
+
 ## Installation
 
 Install using vim-plug:
@@ -14,13 +19,54 @@ Install using packer:
 use {'kiyoon/nvim-tree-remote.nvim'}
 ```
 
-Setup telescope with path actions in vimscript / lua:
+Install `pynvim`.  
+```bash
+pip3 install --user pynvim
+```
+
+Setup Nvim-Tree with remote actions in vimscript / lua:  
 ```vim
 " For lua users, delete the first and the last line.
 lua << EOF
-local remote_actions = require('nvim_tree_remote')
 
-require('nvim-tree').setup {
+-- Remote nvim's --listen address
+vim.g.nvim_tree_remote_socket_path = '/tmp/nvim_tree_remote_socket'
+
+local nvim_tree = require('nvim-tree')
+local nt_remote = require('nvim_tree_remote')
+
+nvim_tree.setup {
+  -- ...
+  view = {
+    mappings = {
+      list = {
+        { key = { "l", "<CR>", "<C-t>", "<2-LeftMouse>" }, action = "remote_tabnew", action_cb = nt_remote.tabnew },
+        { key = { "v", "<C-v>" }, action = "remote_vsplit", action_cb = nt_remote.vsplit },
+        { key = "<C-x>", action = "remote_split", action_cb = nt_remote.split },
+        { key = "o", action = "remote_edit", action_cb = nt_remote.edit },
+        { key = "h", action = "close_node" },
+      },
+    },
+  },
+  remove_keymaps = {
+    'O',
+  },
+  -- ...
 }
 EOF
 ```
+
+## Usage
+
+Run neovim like  
+```bash
+nvim --listen /tmp/nvim_tree_remote_socket
+```
+
+Then, run another nvim with Nvim-Tree.  
+```bash
+nvim .
+```
+
+If you open files from the Nvim-Tree, the remote nvim will open the file.  
+Change `g:nvim_tree_remote_socket_path` to your Nvim-Tree to update the remote path to another Neovim host.
--- lua/nvim_tree_remote/init.lua
+++ lua/nvim_tree_remote/init.lua
@@ -1,3 +1,8 @@
+local status_ok, nt_api = pcall(require, "nvim-tree.api")
+if not status_ok then
+	return
+end
+
 local remote_actions = setmetatable({}, {
   __index = function(_, k)
     error("Key does not exist for 'nvim_tree_remote': " .. tostring(k))
@@ -5,22 +10,51 @@ local remote_actions = setmetatable({}, {
 })
 
 local function script_path()
-   local str = debug.getinfo(2, "S").source:sub(2)
-   return str:match("(.*[/\\])")
+   return debug.getinfo(2, "S").source:sub(2)
 end
 
 local function get_parent_dir(path)
-  return path:match("(.*[/\\])")
+  return vim.fn.fnamemodify(path, ":h")
 end
+
 
 local function python_file(name)
-  return get_parent_dir(get_parent_dir(script_path())) .. "/python/" .. name
+  return get_parent_dir(get_parent_dir(get_parent_dir(script_path()))) .. "/python/" .. name
 end
 
-remote_actions.open_vsplit = function()
-  local path = python_file("nvim_command.py")
-  local nt_abspath = require('nvim-tree.api').get_node_at_cursor().absolute_path
-  os.execute("python3 '" .. path .. "' '" .. vim.g.nvim_tree_remote_socket_path .. "' 'vsp \\'" .. nt_abspath .. "\\''")
+local function remote_nvim_command(command)
+  if vim.g.nvim_tree_remote_socket_path then
+    print(command)
+    local path = python_file("nvim_command.py")
+    local python_host = 'python3'
+    if vim.g.python3_host_prog then
+      python_host = vim.g.python3_host_prog
+    end
+    os.execute("'" .. python_host .. "' '" .. path .. "' '" .. vim.g.nvim_tree_remote_socket_path .. "' '" .. command .. "'")
+  else
+    print("ERROR: g:nvim_tree_remote_socket_path not set")
+  end
+end
+
+
+remote_actions.vsplit = function()
+  local nt_abspath = nt_api.tree.get_node_under_cursor().absolute_path
+  remote_nvim_command("vsplit " .. nt_abspath)
+end
+
+remote_actions.split = function()
+  local nt_abspath = nt_api.tree.get_node_under_cursor().absolute_path
+  remote_nvim_command("split " .. nt_abspath)
+end
+
+remote_actions.tabnew = function()
+  local nt_abspath = nt_api.tree.get_node_under_cursor().absolute_path
+  remote_nvim_command("tabnew " .. nt_abspath)
+end
+
+remote_actions.edit = function()
+  local nt_abspath = nt_api.tree.get_node_under_cursor().absolute_path
+  remote_nvim_command("edit " .. nt_abspath)
 end
 
 return remote_actions
