63fd0819501b2dc39fe206ef5f2c77582ecfc870
Kiyoon Kim <yoonkr33@gmail.com>
2022-12-30 15:08:06 +0000

    keep focus

--- lua/nvim_tree_remote/init.lua
+++ lua/nvim_tree_remote/init.lua
@@ -33,30 +33,48 @@ local function remote_nvim_open(command, path)
     local return_code = os.execute("'" .. python_host .. "' '" .. python_path .. "' '" .. vim.g.nvim_tree_remote_socket_path .. "' '" .. command .. " " .. path .. "' 0")
     if return_code ~= 0 then
       if vim.g.nvim_tree_remote_tmux_pane then
-        if vim.g.nvim_tree_remote_tmux_split == "down" then
-          os.execute("tmux split-window -v -t " .. vim.g.nvim_tree_remote_tmux_pane)
-        elseif vim.g.nvim_tree_remote_tmux_split == "right" then
-          os.execute("tmux split-window -h -t " .. vim.g.nvim_tree_remote_tmux_pane)
-        elseif vim.g.nvim_tree_remote_tmux_split == "left" then
-          os.execute("tmux split-window -h -t " .. vim.g.nvim_tree_remote_tmux_pane)
-          os.execute("tmux swap-pane -L")
-        else
-          os.execute("tmux split-window -v -t " .. vim.g.nvim_tree_remote_tmux_pane)
-          os.execute("tmux swap-pane -U")
+        -- Check current pane
+        local handle = io.popen("tmux display-message -p '#{pane_id}'")
+        if handle ~= nil then
+          local current_pane_id = vim.fn.trim(handle:read("*a"))
+          handle:close()
+
+          -- split pane
+          local new_pane_id = nil
+          if vim.g.nvim_tree_remote_tmux_split == "down" then
+            handle = io.popen("tmux split-window -v -t " .. vim.g.nvim_tree_remote_tmux_pane .. " -P -F '#{pane_id}'")
+          elseif vim.g.nvim_tree_remote_tmux_split == "right" then
+            handle = io.popen("tmux split-window -h -t " .. vim.g.nvim_tree_remote_tmux_pane .. " -P -F '#{pane_id}'")
+          elseif vim.g.nvim_tree_remote_tmux_split == "left" then
+            handle = io.popen("tmux split-window -h -t " .. vim.g.nvim_tree_remote_tmux_pane .. " -P -F '#{pane_id}'")
+            os.execute("tmux swap-pane -L")
+          else
+            handle = io.popen("tmux split-window -v -t " .. vim.g.nvim_tree_remote_tmux_pane .. " -P -F '#{pane_id}'")
+            os.execute("tmux swap-pane -U")
+          end
+          if handle ~= nil then
+            new_pane_id = vim.fn.trim(handle:read("*a"))
+            handle:close()
+          end
+
+          -- focus on the original pane
+          os.execute("tmux select-pane -t '" .. current_pane_id .. "'")
+
+          -- Open nvim
+          if vim.g.nvim_tree_remote_editor_init_file and vim.g.nvim_tree_remote_editor_init_file ~= '' then
+            os.execute("tmux send-keys -t '" .. new_pane_id .. "' 'nvim --listen '")
+            os.execute("tmux send-keys -t '" .. new_pane_id .. "' \\''" .. vim.g.nvim_tree_remote_socket_path .. "'\\'")
+            os.execute("tmux send-keys -t '" .. new_pane_id .. "' ' -u '")
+            os.execute("tmux send-keys -t '" .. new_pane_id .. "' \\''" .. vim.g.nvim_tree_remote_editor_init_file .. "'\\'")
+            os.execute("tmux send-keys -t '" .. new_pane_id .. "' Enter")
+          else
+            --os.execute("tmux send-keys 'nvim --listen '\\'" .. vim.g.nvim_tree_remote_socket_path .. "\\' Enter")
+            os.execute("tmux send-keys -t '" .. new_pane_id .. "' 'nvim --listen '")
+            os.execute("tmux send-keys -t '" .. new_pane_id .. "' \\''" .. vim.g.nvim_tree_remote_socket_path .. "'\\'")
+            os.execute("tmux send-keys -t '" .. new_pane_id .. "' Enter")
+          end
+          os.execute("'" .. python_host .. "' '" .. python_path .. "' '" .. vim.g.nvim_tree_remote_socket_path .. "' 'edit " .. path .. "' 100")
         end
-        if vim.g.nvim_tree_remote_editor_init_file and vim.g.nvim_tree_remote_editor_init_file ~= '' then
-          os.execute("tmux send-keys 'nvim --listen '")
-          os.execute("tmux send-keys \\''" .. vim.g.nvim_tree_remote_socket_path .. "'\\'")
-          os.execute("tmux send-keys ' -u '")
-          os.execute("tmux send-keys \\''" .. vim.g.nvim_tree_remote_editor_init_file .. "'\\'")
-          os.execute("tmux send-keys Enter")
-        else
-          --os.execute("tmux send-keys 'nvim --listen '\\'" .. vim.g.nvim_tree_remote_socket_path .. "\\' Enter")
-          os.execute("tmux send-keys 'nvim --listen '")
-          os.execute("tmux send-keys \\''" .. vim.g.nvim_tree_remote_socket_path .. "'\\'")
-          os.execute("tmux send-keys Enter")
-        end
-        os.execute("'" .. python_host .. "' '" .. python_path .. "' '" .. vim.g.nvim_tree_remote_socket_path .. "' 'edit " .. path .. "' 100")
       else
         print("Error executing command: " .. command)
       end
